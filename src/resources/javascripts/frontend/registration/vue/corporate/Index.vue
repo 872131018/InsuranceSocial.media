3192b89d554689226b81a803c280d83136590889
John Huffman <johnhuffman@Johns-Air.hsd1.co.comcast.net>
2018-01-10 10:16:27 -0700

    updates

--- src/resources/javascripts/frontend/registration/vue/corporate/Index.vue
+++ src/resources/javascripts/frontend/registration/vue/corporate/Index.vue
@@ -10,10 +10,9 @@
         h3 Getting started is easy too.
         h4 The sign-up process takes 10 minutes or less.
         p 1. Click here to get started. If you choose the Essential Plan, {{ company }} will cover the full cost of your subscription through {{ expiration }}. If you choose the Standard or Concierge Plans, {{ company }} will pay for $39 of your total monthly subscription through {{ expiration }}.
-        p 2. Enter your contact information and create a password.
-        p 3. Connect your social media accounts to InsuranceSocial.Media. (If you already have a corporate Facebook page, you’ll enter your personal Facebook login info to allow us to connect to that page. If you don’t have a corporate Facebook page, we’ll set one up for you for a one-time fee of $25.)
-        p 4. Enter your credit card information and {{ company }} code.
-        p 5. Answer a few questions that will enable us to customize your posts to your local market, personal interests and product lines.
+        p 2. Connect your social media accounts to InsuranceSocial.Media. (If you already have a corporate Facebook page, you’ll enter your personal Facebook login info to allow us to connect to that page. If you don’t have a corporate Facebook page, we’ll set one up for you for a one-time fee of $25.)
+        p 3. Enter your credit card information.
+        p 4. Answer a few questions that will enable us to customize your posts to your local market, personal interests and product lines.
         p Activate your account today.
         p #[b *Note: this offer is valid only to new InsuranceSocial.Media subscribers.]
         div(class="w3-padding-16")
